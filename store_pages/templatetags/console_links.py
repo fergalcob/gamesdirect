ae86e644de780a3569635e0ccd0d6dfb0be4031e
John Doe <johndoe@example.com>
2023-08-29 00:37:26 +0100

    Wishlist confirmation tag added to tag library

--- store_pages/templatetags/console_links.py
+++ store_pages/templatetags/console_links.py
@@ -17,3 +17,10 @@ def get_consoles():
 def get_cart(request):
     my_cart = CurrentCart.objects.filter(owner=request.user)
     return my_cart
+
+@register.simple_tag
+def check_wishlist(user, game_value,platform_value):
+    game_to_check = Game.objects.get(Q(game_id=game_value) & Q(platform=platform_value))
+
+    if user.is_authenticated:
+        return Wishlist.objects.filter(Q(owner=user) & Q(wishlist_items__game_id=game_to_check.game_id) & Q(wishlist_items__platform=game_to_check.platform)).exists()
