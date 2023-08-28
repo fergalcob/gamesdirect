5bbc4d3f183c5aaa6b44f3336ec8b0a3ff84eea0
John Doe <johndoe@example.com>
2023-08-29 00:35:28 +0100

    Models added to admin.py

--- store_pages/admin.py
+++ store_pages/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .resource import GameResource  
+from .models import  Game, Videos, Screenshots, Cover, Involved_companies, Company, Console, Genres, Wishlist, CurrentCart, Orders
 
 # Register your models here.
+
+class GameAdmin(ImportExportModelAdmin):
+     resource_class = GameResource  
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Videos)
+admin.site.register(Screenshots)
+admin.site.register(Cover)
+admin.site.register(Involved_companies)
+admin.site.register(Company)
+admin.site.register(Console)
+admin.site.register(Genres)
+admin.site.register(Wishlist)
+admin.site.register(CurrentCart)
+admin.site.register(Orders)
