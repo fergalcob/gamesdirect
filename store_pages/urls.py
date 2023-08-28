5add8e974672d96edbddc160837128a63cac90c2
John Doe <johndoe@example.com>
2023-08-29 00:34:55 +0100

    Store_pages urls.py updated

---
+++ store_pages/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from django.urls import path, include, re_path
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    re_path(r'add_to_cart', views.add_to_cart, name='add_to_cart'),
+    re_path(r'add_to_wishlist', views.add_to_wishlist, name='add_to_wishlist'),
+    re_path(r'remove_from_wishlist', views.remove_from_wishlist, name='remove_from_wishlist'),
+    re_path(r'remove_from_cart', views.remove_from_cart, name='remove_from_cart'),
+    re_path(r'update_cart', views.update_cart, name='update_cart'),
+    path('games', views.games, name='games'),
+    path('products/<str:pk>', views.products, name='products'),
+    path('subscribe', views.subscribe_view, name='subscribe'),
+    path('account/my_orders', views.my_orders, name='my_orders'),
+    path('account/my_wishlist', views.my_wishlist, name='my_wishlist'),
+    path('account/change_details', views.change_details, name='change_details'),
+]
