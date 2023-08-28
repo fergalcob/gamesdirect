0a70dd3edc884ccea1859b424fc61f6287be16c2
John Doe <johndoe@example.com>
2023-08-29 00:27:31 +0100

    Initial checkout view created and API keys added to env

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+from django.http import JsonResponse, HttpResponse
+from store_pages.models import *
+from django.views.generic.base import TemplateView
+from datetime import datetime
+import stripe
+import secrets
+import string
+from django.contrib import messages
+import mailchimp_transactional
+from mailchimp_transactional.api_client import ApiClientError
+import logging
+from store_pages.views import index
