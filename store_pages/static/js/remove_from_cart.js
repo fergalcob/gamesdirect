89606fd0ffd22d516b8a0d6622b01d2fc074484d
John Doe <johndoe@example.com>
2023-08-29 02:36:24 +0100

    Minified JS files

--- store_pages/static/js/remove_from_cart.js
+++ store_pages/static/js/remove_from_cart.js
@@ -1,17 +1,1 @@
-$(document).on("click", "#remove-from-cart", function() {
-        const csrftoken = Cookies.get('csrftoken');
-        let cart_item = $(this).attr("data-product-id");
-        console.log(cart_item)
-        $.ajax({
-            url: 'remove_from_cart',
-            method: 'POST',
-            headers: {'X-CSRFToken': csrftoken},
-            mode: 'same-origin',
-            data: {
-                'cart_item_id': cart_item,
-            },
-            success: function (response) {
-                $('#side-cart-dynamic').load(location.href + " #side-cart-dynamic");
-            },
-        });
-    });
+$(document).on("click","#remove-from-cart",function(){let e=Cookies.get("csrftoken"),t=$(this).attr("data-product-id");console.log(t),$.ajax({url:"remove_from_cart",method:"POST",headers:{"X-CSRFToken":e},mode:"same-origin",data:{cart_item_id:t},success:function(e){$("#side-cart-dynamic").load(location.href+" #side-cart-dynamic")}})});
