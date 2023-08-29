fcd786f1c7abb4d5b5525eed513ba69b0606705c
fergalcob <fergalcob@gmail.com>
2023-08-29 23:02:31 +0100

    Python code validated via Python Linter

--- checkout/views.py
+++ checkout/views.py
@@ -23,19 +23,25 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 @csrf_exempt
 def stripe_config(request):
-    if request.method == 'GET':
-        stripe_config = {'publicKey': settings.STRIPE_PUBLISHABLE_KEY}
+    if request.method == "GET":
+        stripe_config = {"publicKey": settings.STRIPE_PUBLISHABLE_KEY}
         return JsonResponse(stripe_config, safe=False)
-    
+
+
 @csrf_exempt
 def create_checkout_session(request):
-    if request.method == 'GET':
-        domain_url = 'https://www.gamesdirect.shop/'
+    if request.method == "GET":
+        domain_url = "https://www.gamesdirect.shop/"
         stripe.api_key = settings.STRIPE_SECRET_KEY
         user_cart = CurrentCart.objects.get(owner=request.user)
         order_length = 16
-        order_number_generator = ''.join(secrets.choice(string.ascii_uppercase + string.digits)for i in range(order_length))
-        new_order, created = Orders.objects.get_or_create(order_number=order_number_generator)
+        order_number_generator = "".join(
+            secrets.choice(string.ascii_uppercase + string.digits)
+            for i in range(order_length)
+        )
+        new_order, created = Orders.objects.get_or_create(
+            order_number=order_number_generator
+        )
         if created is True:
             new_order.owner_id = user_cart.owner
             new_order.order_items = user_cart.cart_items
@@ -46,16 +52,34 @@ def create_checkout_session(request):
         try:
             checkout_session = stripe.checkout.Session.create(
                 client_reference_id=new_order.order_number,
-                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url=domain_url + 'cancelled/',
-                payment_method_types=['card'],
-                mode='payment',
-                line_items=[{ 'price_data':{'product_data':{'name':'Order #' + str(new_order.order_number), 'description':'Payment to GamesDirect.Shop for Order #' + str(new_order.order_number) + '.'},'currency':'eur','unit_amount_decimal':user_cart.total_price.amount*100},'quantity':1}],          
+                success_url=domain_url
+                + "success?session_id={CHECKOUT_SESSION_ID}",
+                cancel_url=domain_url + "cancelled/",
+                payment_method_types=["card"],
+                mode="payment",
+                line_items=[
+                    {
+                        "price_data": {
+                            "product_data": {
+                                "name": "Order #"
+                                + str(new_order.order_number),
+                                "description": "Payment to GamesDirect.Shop"
+                                " for Order #"
+                                + str(new_order.order_number)
+                                + ".",
+                            },
+                            "currency": "eur",
+                            "unit_amount_decimal": user_cart.total_price.amount
+                            * 100,
+                        },
+                        "quantity": 1,
+                    }
+                ],
             )
 
-            return JsonResponse({'sessionId': checkout_session['id']})
+            return JsonResponse({"sessionId": checkout_session["id"]})
         except Exception as e:
-            return JsonResponse({'error': str(e)})
+            return JsonResponse({"error": str(e)})
 
 
 @csrf_exempt
@@ -63,7 +87,7 @@ def stripe_webhook(request):
     stripe.api_key = settings.STRIPE_SECRET_KEY
     endpoint_secret = settings.STRIPE_ENDPOINT_SECRET
     payload = request.body
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
     event = None
 
     try:
@@ -78,133 +102,168 @@ def stripe_webhook(request):
         return HttpResponse(status=400)
 
     # Handle the checkout.session.completed event
-    if event['type'] == 'checkout.session.completed':
-        status_update = Orders.objects.get(order_number = event['data']['object']['client_reference_id'])
-        empty_cart = CurrentCart.objects.get(owner = status_update.owner_id)
+    if event["type"] == "checkout.session.completed":
+        status_update = Orders.objects.get(
+            order_number=event["data"]["object"]["client_reference_id"]
+        )
+        empty_cart = CurrentCart.objects.get(owner=status_update.owner_id)
         empty_cart.delete()
-        for items in status_update.order_items['current_cart']:
-            items['activation_keys'] = []
-            order_item = Game.objects.get(id = items['item_id'])
-            for number_of_keys in range(0,items['item_quantity']):
+        for items in status_update.order_items["current_cart"]:
+            items["activation_keys"] = []
+            order_item = Game.objects.get(id=items["item_id"])
+            for number_of_keys in range(0, items["item_quantity"]):
                 key = order_item.keys_in_stock[number_of_keys]
-                items['activation_keys'].append(key)
+                items["activation_keys"].append(key)
                 order_item.keys_in_stock.pop(number_of_keys)
                 order_item.save()
         status_update.order_status = "Completed"
         status_update.save()
 
         message = {
-        'from_email': 'support@gamesdirect.shop',
-        'subject': 'Your Order: ' + status_update.order_number,
-        'html': '<p>Hey there,</p> '
-                '<p>We wish to let you know that your order(' + status_update.order_number + ') has been completed. You can find your activation keys at the following URL:</p>'
-                '<p>All the best,</p>'
-                '<p>The GamesDirect Team</p>'
-                '<a href="https://www.gamesdirect.shop">GamesDirect</a>', 
-        'to': [
-            {
-                'email': status_update.owner_id.email,
-                'type': 'to'
-            },
-        ]
+            "from_email": "support@gamesdirect.shop",
+            "subject": "Your Order: " + status_update.order_number,
+            "html": "<p>Hey there,</p> "
+            "<p>We wish to let you know that your order("
+            + status_update.order_number
+            + ") has been completed. You can find your activation keys"
+            " at the following URL:</p>"
+            "<p>All the best,</p>"
+            "<p>The GamesDirect Team</p>"
+            '<a href="https://www.gamesdirect.shop">GamesDirect</a>',
+            "to": [
+                {"email": status_update.owner_id.email, "type": "to"},
+            ],
         }
         try:
-            response = mailchimp.messages.send({
-                'message': message,
-            })
-            return JsonResponse({
-                'detail': 'Email has been sent',
-                'response': response,
-            })
+            response = mailchimp.messages.send(
+                {
+                    "message": message,
+                }
+            )
+            return JsonResponse(
+                {
+                    "detail": "Email has been sent",
+                    "response": response,
+                }
+            )
         except ApiClientError as error:
-            return JsonResponse({
-                'detail': 'Something went wrong',
-                'error': error.text,
-            })
-        
-    elif event['type'] == 'checkout.session.async_payment_succeeded':
-        status_update = Orders.objects.get(order_number = event['data']['object']['client_reference_id'])
-        empty_cart = CurrentCart.objects.get(owner = status_update.owner_id)
+            return JsonResponse(
+                {
+                    "detail": "Something went wrong",
+                    "error": error.text,
+                }
+            )
+
+    elif event["type"] == "checkout.session.async_payment_succeeded":
+        status_update = Orders.objects.get(
+            order_number=event["data"]["object"]["client_reference_id"]
+        )
+        empty_cart = CurrentCart.objects.get(owner=status_update.owner_id)
         empty_cart.delete()
-        for items in status_update.order_items['current_cart']:
-            items['activation_keys'] = []
-            order_item = Game.objects.get(id = items['item_id'])
-            for number_of_keys in range(0,items['item_quantity']):
+        for items in status_update.order_items["current_cart"]:
+            items["activation_keys"] = []
+            order_item = Game.objects.get(id=items["item_id"])
+            for number_of_keys in range(0, items["item_quantity"]):
                 key = order_item.keys_in_stock[number_of_keys]
-                items['activation_keys'].append(key)
+                items["activation_keys"].append(key)
                 order_item.keys_in_stock.pop(number_of_keys)
                 order_item.save()
         status_update.order_status = "Completed"
         status_update.save()
 
         message = {
-        'from_email': 'support@gamesdirect.shop',
-        'subject': 'Your Order: ' + status_update.order_number,
-        'html': '<p>Hey there,</p> '
-                '<p>We wish to let you know that your order(' + status_update.order_number + ') has been completed. You can find your activation keys at the following URL:</p>'
-                '<p>All the best,</p>'
-                '<p>The GamesDirect Team</p>'
-                '<a href="https://www.gamesdirect.shop">GamesDirect</a>', 
-        'to': [
-            {
-                'email': status_update.owner_id.email,
-                'type': 'to'
-            },
-        ]
+            "from_email": "support@gamesdirect.shop",
+            "subject": "Your Order: " + status_update.order_number,
+            "html": "<p>Hey there,</p> "
+            "<p>We wish to let you know that your order("
+            + status_update.order_number
+            + ") has been completed. You can find your activation keys at"
+            " the following URL:</p>"
+            "<p>All the best,</p>"
+            "<p>The GamesDirect Team</p>"
+            '<a href="https://www.gamesdirect.shop">GamesDirect</a>',
+            "to": [
+                {"email": status_update.owner_id.email, "type": "to"},
+            ],
         }
         try:
-            response = mailchimp.messages.send({
-                'message': message,
-            })
-            return JsonResponse({
-                'detail': 'Email has been sent',
-                'response': response,
-            })
+            response = mailchimp.messages.send(
+                {
+                    "message": message,
+                }
+            )
+            return JsonResponse(
+                {
+                    "detail": "Email has been sent",
+                    "response": response,
+                }
+            )
         except ApiClientError as error:
-            return JsonResponse({
-                'detail': 'Something went wrong',
-                'error': error.text,
-            })
-    elif event['type'] == ' checkout.session.async_payment_failed':
-        status_update = Orders.objects.get(order_number = event['data']['object']['client_reference_id'])
+            return JsonResponse(
+                {
+                    "detail": "Something went wrong",
+                    "error": error.text,
+                }
+            )
+    elif event["type"] == " checkout.session.async_payment_failed":
+        status_update = Orders.objects.get(
+            order_number=event["data"]["object"]["client_reference_id"]
+        )
         status_update.order_status = "Payment Failed"
         status_update.save()
 
         message = {
-        'from_email': 'support@gamesdirect.shop',
-        'subject': 'Your Order: ' + status_update.order_number,
-        'html': '<p>Hey there,</p> '
-                '<p>We wish to let you know that the payment for your order(' + status_update.order_number + ') has failed. Your payment method has not been charged and your order has not been completed</p>'
-                '<p>You can try again with a different payment method as your cart has been retained.'
-                '<p>All the best,</p>'
-                '<p>The GamesDirect Team</p>'
-                '<a href="https://www.gamesdirect.shop">GamesDirect</a>', 
-        'to': [
-            {
-                'email': status_update.owner_id.email,
-                'type': 'to'
-            },
-        ]
+            "from_email": "support@gamesdirect.shop",
+            "subject": "Your Order: " + status_update.order_number,
+            "html": "<p>Hey there,</p> "
+            "<p>We wish to let you know that the payment for your order("
+            + status_update.order_number
+            + ") has failed. Your payment method has not been charged and"
+            " your order has not been completed</p>"
+            "<p>You can try again with a different payment method as"
+            " your cart has been retained."
+            "<p>All the best,</p>"
+            "<p>The GamesDirect Team</p>"
+            '<a href="https://www.gamesdirect.shop">GamesDirect</a>',
+            "to": [
+                {"email": status_update.owner_id.email, "type": "to"},
+            ],
         }
         try:
-            response = mailchimp.messages.send({
-                'message': message,
-            })
-            return JsonResponse({
-                'detail': 'Email has been sent',
-                'response': response,
-            })
+            response = mailchimp.messages.send(
+                {
+                    "message": message,
+                }
+            )
+            return JsonResponse(
+                {
+                    "detail": "Email has been sent",
+                    "response": response,
+                }
+            )
         except ApiClientError as error:
-            return JsonResponse({
-                'detail': 'Something went wrong',
-                'error': error.text,
-            })
+            return JsonResponse(
+                {
+                    "detail": "Something went wrong",
+                    "error": error.text,
+                }
+            )
 
     return HttpResponse(status=200)
 
+
 def SuccessView(request):
-    messages.add_message(request, messages.INFO, "Your payment was successful and your items will be delivered shortly.")
-    return redirect('index')
+    messages.add_message(
+        request,
+        messages.INFO,
+        "Your payment was successful and your items will be"
+        " delivered shortly.",
+    )
+    return redirect("index")
+
+
 def CancelledView(request):
-    messages.add_message(request, messages.INFO, "You have cancelled your payment.")
-    return redirect('index')
+    messages.add_message(
+        request, messages.INFO, "You have cancelled your payment."
+    )
+    return redirect("index")
