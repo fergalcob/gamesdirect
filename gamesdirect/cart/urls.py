4d642fccee0205461264d0c7210d08d54d04a704
fergalcob <fergalcob@gmail.com>
2023-10-10 23:01:32 +0100

    Update cart urls

--- gamesdirect/cart/urls.py
+++ gamesdirect/cart/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     re_path(r'add_to_cart', views.add_to_cart, name='add_to_cart'),
     re_path(r'remove_from_cart', views.remove_from_cart, name='remove_from_cart'),
     re_path(r'update_cart', views.update_cart, name='update_cart'),
+    path('account/add_to_cart', views.add_to_cart, name='add_to_cart'),
 ]
